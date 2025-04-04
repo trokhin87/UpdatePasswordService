35d01d4b9d4f37a2942d484bf8fddea2455463e0
Gandoler <gl.krutoi@mail.ru>
2025-04-04 12:03:19 +0300

    r

--- WebLevel/Program.cs
+++ WebLevel/Program.cs
@@ -95,12 +95,7 @@ builder.Services.AddHttpClient<IRepository, Repository>(client =>
     client.BaseAddress = new Uri(dbProxy);
 });
 builder.Services.AddScoped<IMail, MailRepository>();
-builder.Services.AddHttpClient<IService, PasswordRecoveryService>(client =>
-{
-    if (string.IsNullOrEmpty(dbProxy)) throw new Exception("dbProxy не инициализирован");
-    client.BaseAddress = new Uri(dbProxy);
-});
-
+builder.Services.AddScoped<IService, PasswordRecoveryService>();
 
 builder.Services.AddSingleton<TokenRepository>(provider =>
     new TokenRepository(
