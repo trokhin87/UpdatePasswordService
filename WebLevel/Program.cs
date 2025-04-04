af043a43e9b7cd9e81d40b0e564b37b8e13cfb10
Gandoler <gl.krutoi@mail.ru>
2025-04-04 11:56:48 +0300

    200000 поппытка

--- WebLevel/Program.cs
+++ WebLevel/Program.cs
@@ -99,13 +99,20 @@ builder.Services.AddHttpClient<IRepository, Repository>(client =>
     client.BaseAddress = new Uri(dbProxy);
 });
 builder.Services.AddScoped<IMail, MailRepository>();
-builder.Services.AddScoped<IService, PasswordRecoveryService>();
+builder.Services.AddHttpClient<IService, PasswordRecoveryService>(client =>
+{
+    if (string.IsNullOrEmpty(dbProxy)) throw new Exception("dbProxy не инициализирован");
+    client.BaseAddress = new Uri(dbProxy);
+});
+
+
 builder.Services.AddSingleton<TokenRepository>(provider =>
     new TokenRepository(
         jwtSecret,
         provider.GetRequiredService<ILogger<TokenRepository>>()
     ));
 builder.Services.AddLogging();
+
 
 // Swagger
 builder.Services.AddEndpointsApiExplorer();
